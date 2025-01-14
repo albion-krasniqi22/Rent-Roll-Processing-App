8324404ca598d50736002546ba7d57eaee3c3f45
albion-krasniqi22 <102633823+albion-krasniqi22@users.noreply.github.com>
2025-01-14 15:29:28 -0700

    Update rent-roll-processor-v02.py

--- rent-roll-processor-v02.py
+++ rent-roll-processor-v02.py
@@ -190,13 +190,13 @@ def standardization_instructions():
     - Floor Plan Code: Includes variations like "Floor Plan", "Plan Code", "Floorplan", "Unit Type", Bd/Ba, "Type"
     - Net sf: Includes variations like "Sqft", "Unit Sqft", "Square Feet", "Sq. Ft."
     - Occupancy Status / Code: Includes variations like "Unit Status", "Lease Status", "Occupancy", "Unit/Lease Status"
-    - Market Rent: Includes variations like "Market Rent", "Market + Addl.", "Gross Market Rent", "Market"
+    - Market Rent: Includes variations like "Market Rent", "Market + Addl.", "Gross Market Rent", "Market", "Terget Rent"
     - Lease Start Date: Includes variations like "Lease Start", "Lease Start Date", "Start of Lease" (not lease name)
     - Lease Expiration: Includes variations like "Lease End", "Lease End Date", "Lease Expiration Date"
     - Move In Date: Includes variations like "Move-In", "Move In Date", "Move In"
     - Move Out Date: Includes variations like "Move-Out", "Move Out Date", "Move Out"
     - Charge Codes: Includes variations like "Trans Code", "Charge Codes", "Description", "Lease Charges"
-    - Amount: these are charges in dollar amount (which is different from charge code), Charges or credits, Scheduled Charges, Actual charges (do not rename Recurring Charges)
+    - Amount: these are charges in dollar amount (which is different from charge code), Charges or credits, Scheduled Charges, and Actual charges (do not rename Recurring Charges, Monthly Charges, Monthly Rent).
 
     Examples of Standardized Headers:
     Unit No., Floor Plan Code, Sqft, Occupancy Status, Market Rent, Lease Start Date, Lease Expiration, Move In Date, Move-Out Date, Charge Codes
